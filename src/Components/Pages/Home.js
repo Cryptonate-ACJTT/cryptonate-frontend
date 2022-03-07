7d39279a1c52560ad13e2d17ac03b81dcc5eb9ae
Hana Mohideen <hanamohideen@Hanas-MacBook-Pro.local>
2022-03-07 10:48:16 -0500

    home page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import './Home.css'
+import image from './Images/image1.png'
 
 const Home = (props) => {
-    return(<p>Hello</p>);
+    return (
+        <div className="container-center">
+            <div>
+                <h1 className="title">CHANGE THE WORLD <br/>WITH CRYPTONATE</h1>
+                <h className="tagline">blockchain for a better tomorrow</h>
+            <img className="home-screen-image" src={image} alt="img" />
+            </div>
+           
+        </div>
+    )
 }
 
 export default Home;
