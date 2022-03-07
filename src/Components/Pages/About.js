6484687246633a6dcf6e197f9b055c4b34ad522c
Hana Mohideen <hanamohideen@Hanas-MacBook-Pro.local>
2022-03-07 11:18:18 -0500

    about page

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,8 +1,21 @@
 import React from "react";
+import './About.css'
 
 const About = (props) => {
+    const aboutData = {
+        about: "Cryptonate is a donation platform that seeks to minimize many of the challenges and frustrations faced both by nonprofits and those who want to donate. We believe that when accountablility is encouraged, nonprofits become more closely aligned with the goals of their community, are able to attract and hold larger stakeholders, and grow their base of support. Cryptonate facillitates transparency in the donation process by leveraging blocckchain technology to show donors exactly where their money is going, how it’s being spent, and when it’s been used. "
+    }
 
-    return(<p>innit</p>);
+    return(
+        <div className="container-center">
+            <div>
+               <h className="about-title">About Us</h>
+               <h className="about">{aboutData.about}</h>
+            </div>
+           
+        </div>
+    
+        );
 }
 
 export default About;
