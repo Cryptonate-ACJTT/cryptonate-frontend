a0d1cf6a09f94e69579d324f434d4032dc44d4e1
Hana Mohideen <hanamohideen@Hanas-MacBook-Pro.local>
2022-03-09 14:30:42 -0500

    switch to react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,33 +1,74 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-// import BackgroundCanvas from './Components/PageBits/BackgroundCanvas/BackgroundCanvas.js'
+
+
 import SiteHeader from './Components/PageBits/SiteHeader/SiteHeader.js';
 import SiteContent from './Components/PageBits/SiteContent/SiteContent';
 
-//import HomePage from './Components/Pages/Home';
-import Explore from './Components/Pages/Explore';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: <Explore/>
-    }
-  }
+import Home from './Components/Pages/Home'
+import Explore from './Components/Pages/Explore'
+import About from './Components/Pages/About'
+import Crypto from './Components/Pages/WhyCrypto'
+import SignupLogin from './Components/Pages/SignUpLogin'
+import TestRegister from './Components/Pages/TestRegister';
 
-  changePage = (page) => {
-    this.setState({content: page});
-  }
-
-  render() {
-    return ( //background canvas later
-      <div className="App">
-        <SiteHeader onClick={this.changePage}/>
-        <SiteContent content={this.state.content} passDownOnClick={this.changePage}/>
+const App = () => {
+  return (
+    <BrowserRouter>
+     <div className="App">
+       <SiteHeader/>
+      
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/explore" element={<Explore/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/why-crypto" element={<Crypto/>} />
+        <Route path="/login-signup" element={<SignupLogin/>} />
+        <Route path="test-register" element={<TestRegister/>}/>
+      </Routes>
       </div>
-    );
+    </BrowserRouter>
+
+    ) 
   }
-}
+
+
+
+
+// class App extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     content: <Explore/>
+//   //   }
+//   // }
+
+//   // changePage = (page) => {
+//   //   this.setState({content: page});
+//   // }
+
+//   render() {
+//     return ( //background canvas later
+
+//       <Router>
+//         <div className="App">
+//           <SiteHeader/>
+//           {/* <SiteContent content={this.state.content} passDownOnClick={this.changePage}/> */}
+//           </div>
+//         <Routes>
+//           <Route exact path="/" component={Home}/>
+//         </Routes>
+//       </Router>
+      
+//       // <div className="App">
+//       //   <SiteHeader onClick={this.changePage}/>
+//       //   <SiteContent content={this.state.content} passDownOnClick={this.changePage}/>
+//       // </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/Components/PageBits/SiteHeader/SiteHeader.js
+++ src/Components/PageBits/SiteHeader/SiteHeader.js
@@ -10,17 +10,33 @@ import SiteHeaderButton from "./SiteHeaderButton/SiteHeaderButton";
 import WhyCrypto from "../../Pages/WhyCrypto";
 import SignUpLogin from "../../Pages/SignUpLogin";
 
+import { Link } from 'react-router-dom'
+
 const SiteHeader = (props) => {
 
     return (
         <header className="site-hdr">
             <div className="site-hdr-logo-div"/>
             <div className="site-hdr-btn-div">
-                <SiteHeaderButton btnText="Home" onClick={props.onClick} page={<Home/>}/>
-                <SiteHeaderButton btnText="Explore" onClick={props.onClick} page={<Explore/>}/>
-                <SiteHeaderButton btnText="About" onClick={props.onClick} page={<About/>}/>
+                <Link to="/">
+                    <SiteHeaderButton btnText="Home" page={Home}/>
+                </Link>
+                <Link to="/explore">
+                    <SiteHeaderButton btnText="Explore" page={Explore}/>
+                </Link>
+                <Link to="/about">
+                    <SiteHeaderButton btnText="About" page={About}/>
+                </Link>
+                <Link to="/why-crypto">
+                    <SiteHeaderButton btnText="Why Crypto" page={WhyCrypto}/>
+                </Link>
+                <Link to="/login-signup">
+                    <SiteHeaderButton btnText="Login | Sign Up" page={SignUpLogin}/>
+                </Link>
+                
+                {/* <SiteHeaderButton btnText="About" onClick={props.onClick} page={<About/>}/>
                 <SiteHeaderButton btnText="Why Crypto?" onClick={props.onClick} page={<WhyCrypto/>}/>
-                <SiteHeaderButton btnText="Login | Sign Up" onClick={props.onClick} page={<SignUpLogin/>}/>
+                <SiteHeaderButton btnText="Login | Sign Up" onClick={props.onClick} page={<SignUpLogin/>}/> */}
             </div>
         </header>
     );
