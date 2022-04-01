7f23ecf05c6b540aae9edf6b3c1474acecfd4585
Ruby <aidan.russell@stonybrook.edu>
2022-04-01 17:32:53 -0400

    updated SiteHeader so we no longer get those errors

--- src/Components/PageBits/SiteHeader/SiteHeader.js
+++ src/Components/PageBits/SiteHeader/SiteHeader.js
@@ -2,19 +2,7 @@ import React, { useState } from "react";
 import './SiteHeader.css';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
-
-import Home from "../../Pages/Home";
-import Explore from "../../Pages/Explore";
-import About from "../../Pages/About";
-import Profile from "../../Pages/Profile";
-import AuthenticationForm from "../../Pages/AuthenticationForm";
-// import Wallet from "../../Pages/Wallet";
-
-
-
 import SiteHeaderButton from "./SiteHeaderButton/SiteHeaderButton";
-import WhyCrypto from "../../Pages/WhyCrypto";
-import SignUpLogin from "../../Pages/SignUpLogin";
 import image from '../../Pages/Images/cryptonate-logo.png'
 
 
@@ -35,32 +23,32 @@ const SiteHeader = (props) => {
     return (
         <nav className="site-hdr">
             <div className="site-hdr-logo-div">
-                <img class="site-hdr-logo" src={image} alt="img" />
+                <img className="site-hdr-logo" src={image} alt="img" />
                 <Link to="/">
-                    <div id="site-hdr-name" page={Home} >CRYPTONATE</div>
+                    <div id="site-hdr-name">CRYPTONATE</div>
                 </Link>
 
             </div>
             <div className="site-hdr-btn-div">
                 <Link to="/home">
-                    <SiteHeaderButton btnText="Home" page={Home} />
+                    <SiteHeaderButton btnText="Home"/>
                 </Link>
                 <Link to="/explore">
-                    <SiteHeaderButton btnText="Explore" page={Explore} />
+                    <SiteHeaderButton btnText="Explore"/>
                 </Link>
                 <Link to="/about">
-                    <SiteHeaderButton btnText="About" page={About} />
+                    <SiteHeaderButton btnText="About"/>
                 </Link>
                 <Link to="/why-crypto">
-                    <SiteHeaderButton btnText="Why Crypto" page={WhyCrypto} />
+                    <SiteHeaderButton btnText="Why Crypto"/>
                 </Link>
                 {loggedIn ?
-                    <div class="dropdown">
+                    <div className="dropdown">
                         {/* <div class="dropbtn">My Account</div> */}
                         <AccountCircleOutlinedIcon sx={{ml:1, mr:1}}></AccountCircleOutlinedIcon>
 
 
-                        <div class="dropdown-content">
+                        <div className="dropdown-content">
                             <Link to ="/profile"> Profile</Link>
                             <Link to ="/wallet"> Wallet</Link>
                             <Link to ="/authentication-form"> Form</Link>
@@ -69,7 +57,7 @@ const SiteHeader = (props) => {
                     </div>
                     :
                     <Link to="/login-signup">
-                        <SiteHeaderButton btnText="Login | Sign Up" page={SignUpLogin} />
+                        <SiteHeaderButton btnText="Login | Sign Up"/>
                     </Link>
 
                 }
