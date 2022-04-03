e56c08b0a25ed91de5681cc42ca987a36ee670a2
MissingRoad <42961431+MissingRoad@users.noreply.github.com>
2022-04-03 19:51:52 -0400

    Update SignUpLogin.js

--- src/Components/Pages/SignUpLogin.js
+++ src/Components/Pages/SignUpLogin.js
@@ -19,6 +19,71 @@ const SignUpLogin = (props) => {
         toggleSignUpTabClicked(true);
     }
 
+
+
+    const validEmail = (value) => {
+        if (!isEmail(value)) {
+          return (
+            <div className="alert alert-danger" role="alert">
+              This is not a valid email.
+            </div>
+          );
+        }
+      };
+      const vusername = (value) => {
+        if (value.length < 3 || value.length > 20) {
+          return (
+            <div className="alert alert-danger" role="alert">
+              The username must be between 3 and 20 characters.
+            </div>
+          );
+        }
+      };
+      const vpassword = (value) => {
+        if (value.length < 6 || value.length > 40) {
+          return (
+            <div className="alert alert-danger" role="alert">
+              The password must be between 6 and 40 characters.
+            </div>
+          );
+        }
+      };
+      const Register = () => {
+        const form = useRef();
+        const checkBtn = useRef();
+        const [username, setUsername] = useState("");
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [successful, setSuccessful] = useState(false);
+        const { message } = useSelector(state => state.message);
+        const dispatch = useDispatch();
+        const onChangeUsername = (e) => {
+          const username = e.target.value;
+          setUsername(username);
+        };
+        const onChangeEmail = (e) => {
+          const email = e.target.value;
+          setEmail(email);
+        };
+        const onChangePassword = (e) => {
+          const password = e.target.value;
+          setPassword(password);
+        };
+        const handleRegister = (e) => {
+          e.preventDefault();
+          setSuccessful(false);
+          form.current.validateAll();
+          if (checkBtn.current.context._errors.length === 0) {
+            dispatch(register(username, email, password))
+              .then(() => {
+                setSuccessful(true);
+              })
+              .catch(() => {
+                setSuccessful(false);
+              });
+          }
+        };
+
     if (signUpTabClicked || (!signUpTabClicked && !loginTabClicked)) {
 
         return (
@@ -32,12 +97,39 @@ const SignUpLogin = (props) => {
 
                     <div class="signup-login-group basic-group">
 
-                        <div class="user-name input-label">USER NAME</div>
-                        <input class="basic-input"></input>
-                        <div class="email input-label">EMAIL</div>
-                        <input class="basic-input"></input>
-                        <div class="password input-label">PASSWORD</div>
-                        <input class="basic-input" type="password"></input>
+                    <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={onChangeUsername}
+                  validations={[required, vusername]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
 
                         <div class="question-container">
                             <div class="warning">Are you an organization?</div>
@@ -88,7 +180,7 @@ const SignUpLogin = (props) => {
 
 }
 
-
+}
 
 
 export default SignUpLogin;
