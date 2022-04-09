095cc6b2f37d5be23b63f81cb04ce24a657a225e
Hyejeong Yun <gpwjdjd@gmail.com>
2022-04-08 22:45:07 -0400

    signup checkbox added

--- src/Components/Pages/SignUpLogin.js
+++ src/Components/Pages/SignUpLogin.js
@@ -7,10 +7,10 @@ import { CONTENT_TYPES } from "../../Fetch/Fetcher";
 
 const SignUpLogin = (props) => {
 
-    const [loginTabClicked, toggleLoginTabClicked] = useState(false);
-    const [signUpTabClicked, toggleSignUpTabClicked] = useState(false);
+  const [loginTabClicked, toggleLoginTabClicked] = useState(false);
+  const [signUpTabClicked, toggleSignUpTabClicked] = useState(false);
 
-    const [Email, setEmail] = useState("");
+  const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [UserName, setName] = useState("");
   const [ConfirmPasword, setConfirmPasword] = useState("");
@@ -33,107 +33,111 @@ const SignUpLogin = (props) => {
 
   const onSubmitHandler1 = (e) => {
     e.preventDefault();
-    
 
-		let formData1 = new FormData(e.currentTarget);
-		postToBackend(API_ROUTES.BACKEND.REGISTER_USER, formData1, {contentType: CONTENT_TYPES.FORM_DATA});
-	}
+
+    let formData1 = new FormData(e.currentTarget);
+    postToBackend(API_ROUTES.BACKEND.REGISTER_USER, formData1, { contentType: CONTENT_TYPES.FORM_DATA });
+  }
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    
 
-		let formData2 = new FormData(e.currentTarget);
-		postToBackend(API_ROUTES.BACKEND.LOGIN_USER, formData2, {contentType: CONTENT_TYPES.FORM_DATA});
+
+    let formData2 = new FormData(e.currentTarget);
+    postToBackend(API_ROUTES.BACKEND.LOGIN_USER, formData2, { contentType: CONTENT_TYPES.FORM_DATA });
   }
-    const handleLoginTabClicked = async (e) => {
+  const handleLoginTabClicked = async (e) => {
 
-        toggleSignUpTabClicked(false);
-        toggleLoginTabClicked(true);
-    }
+    toggleSignUpTabClicked(false);
+    toggleLoginTabClicked(true);
+  }
 
-    const handleSignUpTabClicked = async (e) => {
+  const handleSignUpTabClicked = async (e) => {
 
-        toggleLoginTabClicked(false);
-        toggleSignUpTabClicked(true);
-    }
+    toggleLoginTabClicked(false);
+    toggleSignUpTabClicked(true);
+  }
 
-    if (signUpTabClicked || (!signUpTabClicked && !loginTabClicked)) {
+  if (signUpTabClicked || (!signUpTabClicked && !loginTabClicked)) {
 
-        return (
-            <div class="basic-div basic-form">
+    return (
+      <div class="basic-div basic-form">
 
-                <div id="signup">
-                    <div class="tab-container">
-                        <h id="login-tab" class="basic-tab not-active" onClick={handleLoginTabClicked} >LOGIN</h>
-                        <h id="signup-tab" class="basic-tab active" onClick={handleSignUpTabClicked}> SIGN UP</h>
-                    </div>
+        <div id="signup">
+          <div class="tab-container">
+            <h id="login-tab" class="basic-tab not-active" onClick={handleLoginTabClicked} >LOGIN</h>
+            <h id="signup-tab" class="basic-tab active" onClick={handleSignUpTabClicked}> SIGN UP</h>
+          </div>
 
-                    <div class="signup-login-group basic-group">
+          <div class="signup-login-group basic-group">
 
-                    <form
-        onSubmit={onSubmitHandler1}
-        style={{ display: "flex", flexDirection: "column" }}>
-        <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
+            <form
+              onSubmit={onSubmitHandler1}
+              style={{ display: "flex", flexDirection: "column" }}>
+              <label>Email</label>
+              <input type="email" value={Email} onChange={onEmailHandler} />
 
-        <label>UserName</label>
-        <input type="test" value={UserName} onChange={onUserNamerHandler} />
+              <label>UserName</label>
+              <input type="test" value={UserName} onChange={onUserNamerHandler} />
 
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHanlder} />
+              <label>Password</label>
+              <input type="password" value={Password} onChange={onPasswordHanlder} />
 
-        <label>ConfirmPasword</label>
-        <input
-          type="password"
-          value={ConfirmPasword}
-          onChange={onConfirmPasswordHandler}
-        />
-        <br />
-        <button type="submit">Sign Up</button>
-      </form>
-                    </div>
-                </div>
-            </div>);
-    }
-    else if (loginTabClicked) {
+              <label>ConfirmPasword</label>
+              <input
+                type="password"
+                value={ConfirmPasword}
+                onChange={onConfirmPasswordHandler}
+              />
+              <div class="question-container">
+                <div class="are-you-an-organization">Are you an organization?</div>
+                <input type="checkbox"></input>
+              </div>
+              <br />
+              <button type="submit">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>);
+  }
+  else if (loginTabClicked) {
 
 
-        return (
-            <div class="basic-div basic-form">
+    return (
+      <div class="basic-div basic-form">
 
-                <div id="login" >
+        <div id="login" >
 
-                    <div class="tab-container">
-                        <h class="basic-tab active" onClick={handleLoginTabClicked} >LOGIN</h>
-                        <h class="basic-tab not-active" onClick={handleSignUpTabClicked} >SIGN UP</h>
-                    </div>
+          <div class="tab-container">
+            <h class="basic-tab active" onClick={handleLoginTabClicked} >LOGIN</h>
+            <h class="basic-tab not-active" onClick={handleSignUpTabClicked} >SIGN UP</h>
+          </div>
 
-                    <div class="signup-login-group basic-group">
+          <div class="signup-login-group basic-group">
 
-                    <form
-        onSubmit={onSubmitHandler}
-        style={{ display: "flex", flexDirection: "column" }}>
-        <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
+            <form
+              onSubmit={onSubmitHandler}
+              style={{ display: "flex", flexDirection: "column" }}>
+              <label>Email</label>
+              <input type="email" value={Email} onChange={onEmailHandler} />
 
-      
 
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHanlder} />
 
-        
-        <br />
-        <button type="submit">Login</button>
-      </form>
-                    </div>
-                </div>
-            </div>
-        );
+              <label>Password</label>
+              <input type="password" value={Password} onChange={onPasswordHanlder} />
 
-    } else {
-		return null;
-	}
-    
+
+              <br />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+
+  } else {
+    return null;
+  }
+
 
 
 
